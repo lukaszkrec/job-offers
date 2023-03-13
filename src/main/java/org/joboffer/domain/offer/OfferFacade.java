7c640e9306b0f0455268410b7d05c5f7db1e16c4
Łukasz Kręc <lukasz.krec@onet.pl>
2023-03-13 18:33:31 +0100

    create OfferFacade implementation for module

--- src/main/java/org/joboffer/domain/offer/OfferFacade.java
+++ src/main/java/org/joboffer/domain/offer/OfferFacade.java
@@ -1,6 +1,29 @@
 package org.joboffer.domain.offer;
 
-class OfferFacade {
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+@AllArgsConstructor
+public class OfferFacade {
+
+    private final OfferRepositoryImpl repositoryImpl;
+
+    public Map<String, Offer> findAllOffers() {
+        return repositoryImpl.findAllOffers();
+    }
+
+    public Offer findOfferById(String offerId) {
+        return repositoryImpl.findOfferById(offerId);
+    }
+
+    public Offer save(Offer offer) {
+        return repositoryImpl.save(offer);
+    }
+
+    public void fetchAllOffersAndSaveAllIffNotExist(Map<String, Offer> offers) {
+
+    }
 
 
 }
