3e97d7e6cb6f435c1fc8492671dcede192c605a0
Łukasz Kręc <lukasz.krec@onet.pl>
2023-03-15 18:53:44 +0100

    refactor OfferRepositoryTestImpl and update test for offers

--- src/test/java/org/joboffer/domain/offer/OfferFacadeTest.java
+++ src/test/java/org/joboffer/domain/offer/OfferFacadeTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatRuntimeException;
+import static org.joboffer.domain.offer.OfferMapper.mapFetchedOfferDtoToOffer;
 import static org.joboffer.domain.offer.OfferMapper.mapToOfferDto;
 
 class OfferFacadeTest {
@@ -15,35 +17,55 @@ class OfferFacadeTest {
     private final OfferFacade offerFacade = new OfferFacade(repository, validation);
 
     @Test
-    void should_return_list_of_offers_containing_two_offers() {
+    void should_save_two_offers_when_there_are_no_offers_in_database() {
         //given
         Offer offer1 = new Offer("1L", "https://www.example.com", "Developer", "Apfel", "10000");
         Offer offer2 = new Offer("2L", "https://www.example1.com", "HR", "Amazong", "55555");
-        offerFacade.save(offer1);
-        offerFacade.save(offer2);
-
-        //when
         OfferDto offerDto1 = mapToOfferDto(offer1);
         OfferDto offerDto2 = mapToOfferDto(offer2);
+
+        //when
+        offerFacade.save(offerDto1);
+        offerFacade.save(offerDto2);
         List<OfferDto> allOffers = offerFacade.findAllOffers();
 
         //then
-        assertThat(allOffers).hasSize(2).contains(offerDto1, offerDto2);
+        assertThat(allOffers).hasSize(2);
 
     }
 
     @Test
-    void should_return_an_offer_if_an_offer_with_the_given_id_exist() {
+    void should_throw_exception_when_repository_already_had_offers_with_the_same_url() {
+        //given
+        Offer offer1 = new Offer("1L", "https://www.example.com", "Developer", "Apfel", "10000");
+        Offer offer2 = new Offer("2L", "https://www.example1.com", "HR", "Amazong", "55555");
+        Offer offer3 = new Offer("3L", "https://www.example1.com", "Scrum Master", "GLob", "659832");
+        OfferDto offerDto1 = mapToOfferDto(offer1);
+        OfferDto offerDto2 = mapToOfferDto(offer2);
+        OfferDto offerDto3 = mapToOfferDto(offer3);
+        offerFacade.save(offerDto1);
+        offerFacade.save(offerDto2);
+
+        //when
+        //then
+        assertThatRuntimeException()
+                .isThrownBy(() -> offerFacade.save(offerDto3))
+                .withMessage("Offer with the same url: " + offerDto3.getUrl() + " already exists");
+    }
+
+    @Test
+    void should_return_an_offer_if_offer_with_the_given_id_exist() {
         //given
         String searchedOfferId = "1L";
-        Offer offer1 = new Offer("1L", "https://www.example.com", "Developer", "Apfel", "10000");
-        offerFacade.save(offer1);
+        Offer offer = new Offer("1L", "https://www.example.com", "Developer", "Apfel", "10000");
+        OfferDto offerDto = mapToOfferDto(offer);
+        offerFacade.save(offerDto);
 
         //when
         OfferDto offerById = offerFacade.findOfferById(searchedOfferId);
 
         //then
-        assertThat(offerById).usingRecursiveComparison().isEqualTo(offer1);
+        assertThat(offerById).usingRecursiveComparison().isEqualTo(offer);
 
     }
 
@@ -51,14 +73,15 @@ class OfferFacadeTest {
     void should_throw_an_exception_when_offer_with_the_given_id_does_not_exist() {
         //given
         String searchedOfferId = "2L";
-        Offer offer1 = new Offer("1L", "https://www.example.com", "Developer", "Apfel", "10000");
-        offerFacade.save(offer1);
+        Offer offer = new Offer("1L", "https://www.example.com", "Developer", "Apfel", "10000");
+        OfferDto offerDto = mapToOfferDto(offer);
+        offerFacade.save(offerDto);
 
         //when
         //then
-        assertThatIllegalArgumentException()
+        assertThatRuntimeException()
                 .isThrownBy(() -> offerFacade.findOfferById(searchedOfferId))
-                .withMessage("Offer with id " + searchedOfferId + " does not exist");
+                .withMessage("Offer with id: " + searchedOfferId + " does not exist");
 
     }
 
@@ -66,39 +89,44 @@ class OfferFacadeTest {
     void should_save_offer_when_given_params_are_correct() {
         //given
         Offer offer = new Offer("1L", "https://www.example.com", "Developer", "Apfel", "10000");
+        OfferDto offerDto = mapToOfferDto(offer);
 
         //when
-        OfferDto savedOffer = offerFacade.save(offer);
+        offerFacade.save(offerDto);
         List<OfferDto> allOffers = offerFacade.findAllOffers();
+
         //then
-        assertThat(allOffers).contains(savedOffer);
+        assertThat(allOffers).contains(offerDto);
     }
 
     @Test
     void should_throw_an_exception_when_saved_offer_has_null_id() {
         //given
         Offer offer = new Offer(null, "https://www.example.com", "Developer", "Apfel", "10000");
+        OfferDto offerDto = mapToOfferDto(offer);
 
         //when
         //then
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> offerFacade.save(offer))
-                .withMessage("Offer must have an id");
+        assertThatRuntimeException()
+                .isThrownBy(() -> offerFacade.save(offerDto))
+                .withMessage("Offer id can not be: " + null);
     }
 
     @Test
     void should_throw_an_exception_when_saved_offer_has_same_id_as_existing_offer() {
         //given
-        Offer offer1 = new Offer("1L", "https://www.example.com", "Developer", "Apfel", "10000");
-        Offer offer2 = new Offer("1L", "https://www.example.com", "Developer", "Apfel", "10000");
+        Offer offer1 = new Offer("1L", "https://www.example1.com", "Developer", "Apfel", "10000");
+        Offer offer2 = new Offer("1L", "https://www.example2.com", "Developer", "Apfel", "10000");
+        OfferDto offerDto1 = mapToOfferDto(offer1);
+        OfferDto offerDto2 = mapToOfferDto(offer2);
 
         //when
         //then
-        assertThatIllegalArgumentException()
+        assertThatRuntimeException()
                 .isThrownBy(() -> {
-                    offerFacade.save(offer1);
-                    offerFacade.save(offer2);
-                }).withMessage("Offer with id " + offer2.getId() + " already exists");
+                    offerFacade.save(offerDto1);
+                    offerFacade.save(offerDto2);
+                }).withMessage("Offer with id: " + offer2.getId() + " already exists");
     }
 
     @Test
@@ -106,9 +134,11 @@ class OfferFacadeTest {
         //given
         Offer offer1 = new Offer("1L", "https://www.example1.com", "Developer", "Apfel", "10000");
         Offer offer2 = new Offer("2L", "https://www.example2.com", "HR", "Gogiel", "5000");
+        OfferDto offerDto1 = mapToOfferDto(offer1);
+        OfferDto offerDto2 = mapToOfferDto(offer2);
 
-        offerFacade.save(offer1);
-        offerFacade.save(offer2);
+        offerFacade.save(offerDto1);
+        offerFacade.save(offerDto2);
 
         //when
         Offer duplicateOffer = new Offer("2L", "https://www.example2.com", "HR", "Gogiel", "5000");
@@ -118,7 +148,9 @@ class OfferFacadeTest {
         OfferDto offerDto4 = mapToOfferDto(offer4);
 
         List<OfferDto> offersToFetch = List.of(duplicateOfferDto, offerDto4);
-        offerFacade.fetchAllOffersAndSaveAllIfNotExist(offersToFetch);
+        List<Offer> mappedOffers = mapFetchedOfferDtoToOffer(offersToFetch);
+
+        offerFacade.fetchAllOffersAndSaveAllIfNotExist(mappedOffers);
         List<OfferDto> offers = offerFacade.findAllOffers();
 
         //then
--- src/test/java/org/joboffer/domain/offer/OfferRepositoryTestImpl.java
+++ src/test/java/org/joboffer/domain/offer/OfferRepositoryTestImpl.java
@@ -3,10 +3,16 @@ package org.joboffer.domain.offer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 class OfferRepositoryTestImpl implements OfferRepository {
 
     private final Map<String, Offer> offers = new HashMap<>();
+
+    @Override
+    public Offer save(Offer offerToSave) {
+        return offers.put(offerToSave.getId(), offerToSave);
+    }
 
     @Override
     public List<Offer> findAllOffers() {
@@ -15,64 +21,19 @@ class OfferRepositoryTestImpl implements OfferRepository {
 
 
     @Override
-    public Offer findOfferById(String offerId) {
-        checkingIfOfferWithIdDoesExist(offerId);
-        return offers.get(offerId);
-    }
-
-
-    @Override
-    public Offer save(Offer savedOffer) {
-        checkingIfTheOfferIdIsNotNull(savedOffer);
-        checkingIfAnOfferWithTheSameIdExist(savedOffer);
-        offers.put(savedOffer.getId(), savedOffer);
-        return savedOffer;
-
-    }
-
-    @Override
-    public void fetchAllOffersAndSaveAllIfNotExist(Map<String, Offer> offerDtos) {
-        offerDtos.values().stream()
-                .filter(this::offerDoesNotExists)
-                .forEach(offer -> offers.put(offer.getId(), offer));
-    }
-
-    boolean offerDoesNotExists(Offer offer) {
-        return offers.values()
+    public Optional<Offer> findOfferById(String offerId) {
+        return offers.entrySet()
                 .stream()
-                .noneMatch(existingOffer -> existingOffer.equals(offer));
+                .filter(entry -> entry.getKey().equals(offerId))
+                .map(Map.Entry::getValue)
+                .findFirst();
     }
 
-    boolean offerIdIsNotNull(Offer offer) {
-        return offer.getId() == null;
-    }
 
-    boolean offerExistsWithSameId(String offerId) {
-        return offers.keySet().stream()
-                .anyMatch(id -> id.equals(offerId));
-
-    }
-
-    boolean offerWithIdDoesNotExist(String offerId) {
-        return offers.keySet().stream()
-                .noneMatch(id -> id.equals(offerId));
-    }
-
-    void checkingIfAnOfferWithTheSameIdExist(Offer savedOffer) {
-        if (offerExistsWithSameId(savedOffer.getId())) {
-            throw new OfferAlreadyExistException("Offer with id " + savedOffer.getId() + " already exists");
-        }
-    }
-
-    void checkingIfTheOfferIdIsNotNull(Offer savedOffer) {
-        if (offerIdIsNotNull(savedOffer)) {
-            throw new IllegalArgumentException("Offer must have an id");
-        }
-    }
-
-    void checkingIfOfferWithIdDoesExist(String offerId) {
-        if (offerWithIdDoesNotExist(offerId)) {
-            throw new OfferDoesNotExist("Offer with id " + offerId + " does not exist");
-        }
+    @Override
+    public List<Offer> fetchAllOffersAndSaveAllIfNotExist(List<Offer> offerList) {
+        return offerList.stream()
+                .map(this::save)
+                .toList();
     }
 }
