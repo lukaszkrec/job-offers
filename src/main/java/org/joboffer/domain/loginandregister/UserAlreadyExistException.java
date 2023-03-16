fba93a25424f3b9fdfbdb88b0b684b0f5b5ffb99
Łukasz Kręc <lukasz.krec@onet.pl>
2023-03-16 11:40:05 +0100

    deleted UserAlreadyExistException and UserDoesNotExistException and replaced them to UserValidationException

--- src/main/java/org/joboffer/domain/loginandregister/UserAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package org.joboffer.domain.loginandregister;
-
-public class UserAlreadyExistException extends IllegalArgumentException {
-    public UserAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/joboffer/domain/loginandregister/UserDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package org.joboffer.domain.loginandregister;
-
-public class UserDoesNotExistException extends IllegalArgumentException {
-    public UserDoesNotExistException(String message) {
-        super(message);
-    }
-}
