5839f308f5071207b30d28bfe8b7b0fb3fbfec2b
Łukasz Kręc <lukasz.krec@onet.pl>
2023-03-17 16:25:26 +0100

    added docker-compose, added dependencies for integration tests and added spring boot

---
+++ src/main/java/org/joboffer/JobOfferSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.joboffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class JobOfferSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOfferSpringBootApplication.class, args);
+    }
+
+}
