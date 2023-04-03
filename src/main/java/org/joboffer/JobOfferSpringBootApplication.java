7bb251ce5b69ce091a1512f2e18f723366a04896
Łukasz Kręc <lukasz.krec@onet.pl>
2023-04-03 22:11:24 +0200

    added spring scheduler configuration

--- src/main/java/org/joboffer/JobOfferSpringBootApplication.java
+++ src/main/java/org/joboffer/JobOfferSpringBootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties(HttpClientConnectionConfigurationProperties.class)
-class JobOfferSpringBootApplication {
+public class JobOfferSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JobOfferSpringBootApplication.class, args);
