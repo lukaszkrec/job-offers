63a5f41cae95cd66cce86335d59134cbf6dea1a0
Łukasz Kręc <lukasz.krec@onet.pl>
2023-04-03 22:12:48 +0200

    created integration test for scheduler and added awaitility dependency

---
+++ src/integration/java/org/joboffer/scheduler/OfferFetcherScheduleIntegrationTest.java
@@ -0,0 +1,27 @@
+package org.joboffer.scheduler;
+
+import org.joboffer.BaseIntegrationTest;
+import org.joboffer.JobOfferSpringBootApplication;
+import org.joboffer.domain.offer.OfferFetcher;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.time.Duration;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest(classes = JobOfferSpringBootApplication.class, properties = "scheduling.enabled=true")
+class OfferFetcherScheduleIntegrationTest extends BaseIntegrationTest {
+
+    @SpyBean
+    OfferFetcher offerFetcher;
+
+    @Test
+    void offerFetcherSchedulerCall() {
+        await().atMost(Duration.ofSeconds(2))
+                .untilAsserted(() -> verify(offerFetcher, times(2)).fetchAllOffers());
+    }
+}
